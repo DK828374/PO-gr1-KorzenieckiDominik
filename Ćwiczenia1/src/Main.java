b65558ae0614c08e43d09ccd0994f9c4b64e8943
DK828374 <92021462+DK828374@users.noreply.github.com>
2021-10-11 14:53:15 +0200

    Zadania 06.10.2021

---
+++ Ćwiczenia1/src/Main.java
@@ -0,0 +1,109 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Zad1
+
+        System.out.println(31 + 29 + 31);
+        //Zad2
+        int suma = 0;
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+
+        }
+        System.out.println(suma);
+        //Zad3
+        int iloczyn = 1;
+        for (int i = 1; i <= 10; i++) {
+            iloczyn *= i;
+
+        }
+        System.out.println(iloczyn);
+        //Zad4
+        float stanKonta = 1000;
+        for (int i = 1; i <= 3; i++) {
+            stanKonta += stanKonta * 0.06;
+            System.out.println("Saldo po " + i + " roku/latach wynosi " + stanKonta);
+
+
+        }
+        //Zad5
+
+        System.out.println("--------\n| JAVA |\n--------");
+
+        //Zad6
+        System.out.println("   /////  \n  +\"\"\"\"\"+\n(| o   o |)\n   | ^ | \n  | '_' |\n  +-----+");
+        //Zad7
+
+        System.out.println("" +
+                "* * *                                         *                  *     *       \n" +
+                "*     *                                                                *       \n" +
+                "*      *                                                               *      *\n" +
+                "*       *      * * *      * * * * * * * *     *     * * * *      *     *    *  \n" +
+                "*       *    *       *    *      *      *     *     *      *     *     * *     \n" +
+                "*       *   *         *   *      *      *     *     *      *     *     *   *   \n" +
+                "*      *     *       *    *      *      *     *     *      *     *     *     * \n" +
+                "* * * *        * * *      *      *      *     *     *      *     *     *       *");
+
+
+        //Zad8
+        System.out.println("    +    \n" +
+                           "   + +   \n" +
+                           "  +   *  \n" +
+                           " +-----+ \n" +
+                           " | .-. | \n" +
+                           " | | | | \n" +
+                           " +-+-+-+ ");
+
+        //Zad9
+
+
+        System.out.println("" +
+                "       /\\/\\      ----------     \n" +
+                "      (0  0)    /          \\\n" +
+                "      /    \\   <   HELLO!   |\n" +
+                "      \\    /    \\          /\n" +
+                "       | | |     ----------   \n" +
+                "      (__|__)");
+
+        //Zad10
+        System.out.println("1.Interstellar\n2.Star wars - Rogue One\n3.Obecność\n\n");
+
+        //Zad11
+        System.out.println("Wzgardziłaś mną, dziedziczko moja ucieszona.\n" +
+                "Zdałać sie ojca twego barziej uszczuplona\n" +
+                "Ojczyzna, niżlibyś ty przestać na niej miała;\n" +
+                "To prawda, żeby była nigdy nie zrownała\n" +
+                "Z ranym rozumem twoim, z pięknemi przymioty,\n" +
+                "Z ktorych sie już znaczyły twoje przyszłe cnoty\n" +
+                "O słowa, o zabawo, o wdzięczne ukłony,\n" +
+                "Jakożem ja dziś po was wielce zasmęcony.\n" +
+                "A ty, pociecho moja, już mi sie nie wrócisz\n" +
+                "Na wieki ani mojej tesknice okrócisz.\n" +
+                "Nie lza, nie lza, jedno sie za tobą gotować,\n" +
+                "A stopeczkami twemi ciebie naszladować.\n" +
+                "Tam cię ujźrzę, da Pan Bóg, a ty więc z drogiemi\n" +
+                "Rzuć sie ojcu do szyje ręczynkami swemi.\n");
+
+        //Zad12
+        System.out.println("*     *     *     *     *     * ----------------------------------------------------------------\n" +
+                "   *     *     *     *     *\n" +
+                "*     *     *     *     *     * ----------------------------------------------------------------\n" +
+                "   *     *     *     *     *\n" +
+                "*     *     *     *     *     * ----------------------------------------------------------------\n" +
+                "   *     *     *     *     *\n" +
+                "*     *     *     *     *     * ----------------------------------------------------------------\n" +
+                "   *     *     *     *     *\n" +
+                "*     *     *     *     *     * ----------------------------------------------------------------\n\n" +
+                "------------------------------------------------------------------------------------------------\n\n" +
+                "------------------------------------------------------------------------------------------------\n\n" +
+                "------------------------------------------------------------------------------------------------\n\n" +
+                "------------------------------------------------------------------------------------------------\n\n" +
+                "------------------------------------------------------------------------------------------------\n\n");
+
+
+
+    }
+
+}
