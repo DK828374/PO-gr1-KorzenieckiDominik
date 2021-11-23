8eb9522c544bd3049d282ebfa669147efc844b27
DK828374 <92021462+DK828374@users.noreply.github.com>
2021-11-23 11:51:35 +0100

    Add files via upload

---
+++ Ćwiczenia6/src/Main.java
@@ -0,0 +1,154 @@
+package com.company;
+import java.util.Arrays;
+
+
+
+// zad1   ///////////////////////////////
+
+class RachunekBankowy {
+
+
+    private static double rocznaStopaProcentowa;
+    private double saldo;
+
+
+    RachunekBankowy(double saldo){
+        this.saldo = saldo;
+
+
+    }
+
+    public double obliczMiesieczneOdsetki(double saldo){
+        double odsetki = saldo*rocznaStopaProcentowa/12;
+        this.saldo +=odsetki;
+        return odsetki;
+
+    }
+
+    public void setRocznaStopaProcentowa(double rocznaStopaProcentowa) {
+        RachunekBankowy.rocznaStopaProcentowa = rocznaStopaProcentowa;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
+
+
+// zad2   ///////////////////////////////
+
+
+class IntegerSet {
+
+    private final boolean[] set;
+
+    IntegerSet() {
+        this.set = new boolean[100];
+        Arrays.fill(this.set, false);
+    }
+
+    public static IntegerSet union(IntegerSet set1, IntegerSet set2){
+        IntegerSet set = new IntegerSet();
+        for(int i = 0; i<=set1.set.length-1; i++){
+            if(set1.set[i]  || set2.set[i]){
+                set.set[i] = true;
+            }
+            if(!set1.set[i] && !set2.set[i]){
+                set.set[i] = false;
+            }
+        }
+        return set;
+    }
+
+    public static IntegerSet intersection(IntegerSet set1,IntegerSet set2){
+
+        IntegerSet set = new IntegerSet();
+        for(int i = 0; i<=set1.set.length-1; i++){
+            if(set1.set[i] == set2.set[i]){
+                set.set[i] = set1.set[i];
+            }
+            if(set1.set[i] != set2.set[i])
+                set.set[i] = false;
+        }
+        return set;
+    }
+
+
+    public void insertElement(int n){
+        if(n<=100)
+            this.set[n-1] = true;
+    }
+
+    public void deleteElement(int n){
+        if(n <= 100)
+            this.set[n-1] = false;
+    }
+
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+
+        for(int i = 0; i <= this.set.length-1; i++){
+            if(this.set[i])
+                str.append(i).append(1).append(" ");
+        }return str.toString();
+    }
+
+    public  boolean equals(IntegerSet set2) {
+        boolean spr = true;
+
+        if (this.set.length == set2.set.length) {
+            for (int i = 0; i <= this.set.length-1; i++) {
+                if (this.set[i] != set2.set[i]) {
+                    spr = false;
+                    break;
+                }
+            }
+        }return spr;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        //ZADANIE1
+
+        RachunekBankowy saver1 = new RachunekBankowy(2000);
+        RachunekBankowy saver2 = new RachunekBankowy(3000);
+        saver1.setRocznaStopaProcentowa(0.04);
+        saver2.setRocznaStopaProcentowa(0.04);
+        System.out.println(saver1.obliczMiesieczneOdsetki(saver1.getSaldo()));
+        System.out.println(saver2.obliczMiesieczneOdsetki(saver2.getSaldo()));
+        System.out.println(saver1.getSaldo());
+        System.out.println(saver2.getSaldo());
+        saver1.setRocznaStopaProcentowa(0.05);
+        saver2.setRocznaStopaProcentowa(0.05);
+        System.out.println(saver1.obliczMiesieczneOdsetki(saver1.getSaldo()));
+        System.out.println(saver2.obliczMiesieczneOdsetki(saver2.getSaldo()));
+        System.out.println(saver1.getSaldo());
+        System.out.println(saver2.getSaldo());
+
+        //ZADANIE2
+
+         IntegerSet set1 = new IntegerSet();
+        IntegerSet set2 = new IntegerSet();
+        IntegerSet set3 = new IntegerSet();
+        set1.insertElement(10);
+        set1.insertElement(15);
+        set1.insertElement(20);
+        set2.insertElement(10);
+        set2.insertElement(14);
+        set2.insertElement(25);
+        set3.insertElement(10);
+        set3.insertElement(15);
+        set3.insertElement(20);
+        System.out.println(set1);
+        System.out.println(set2);
+        set2.deleteElement(14);
+        System.out.println(set2);
+        System.out.println(IntegerSet.union(set1, set2));
+        System.out.println((IntegerSet.intersection(set1, set2)));
+        System.out.println(set1.equals(set3));
+        System.out.println(set1.equals(set2));
+
+    }
+}
